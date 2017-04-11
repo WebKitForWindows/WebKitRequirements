d5ceddeda34159dd63baaad0464e5cb0a20a25d0
Don <don.j.olmstead@gmail.com>
2017-04-10 18:52:11 -0700

    feat(build): Add script that runs all steps

---
+++ Run-All.ps1
@@ -0,0 +1,46 @@
+
+Param (
+  # Common options
+  [Parameter()]
+  [string] $sourcePath = 'src',
+  [Parameter()]
+  [string] $buildPath = 'build',
+  [Parameter()]
+  [string] $installationPath = 'dist',
+
+  # Build-RequirementSource options
+  [Parameter()]
+  [ValidateSet('Release','Debug')]
+  [string] $buildType = 'Release',
+  [Parameter()]
+  [ValidateSet('ninja','vs2015','vs2017')]
+  [string] $generator = 'ninja',
+  [Parameter()]
+  [string] $platform = 'Windows'
+)
+
+#----------------------------------------------------------------------
+# Get
+#----------------------------------------------------------------------
+
+& (Join-Path $PSScriptRoot Get-RequirementSource.ps1) -Root $sourcePath;
+
+#----------------------------------------------------------------------
+# Patch
+#----------------------------------------------------------------------
+
+& (Join-Path $PSScriptRoot Patch-RequirementSource.ps1) -Root $sourcePath;
+
+#----------------------------------------------------------------------
+# Build
+#----------------------------------------------------------------------
+
+$args = @{
+  Root = $sourcePath;
+  InstallPath = $installationPath;
+  BuildType = $buildType;
+  Generator = $generator;
+  Platform = $platform;
+}
+
+& (Join-Path $PSScriptRoot Build-RequirementSource) @args
