28dfb664cc9f85a2d95c1aea3e28bc8d7ab95c9d
Don <don.j.olmstead@gmail.com>
2017-08-18 14:29:30 -0700

    Update to nghttp2 v1.25.0

--- Get-RequirementSource.ps1
+++ Get-RequirementSource.ps1
@@ -28,7 +28,7 @@ $zlibUrl = ('https://github.com/madler/zlib/archive/v{0}.zip' -f $zlibVersion);
 $libreSSLVersion = '2.5.5';
 $libreSSLUrl = ('http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-{0}.tar.gz' -f $libreSSLVersion);
 
-$nghttp2Version = '1.21.1';
+$nghttp2Version = '1.25.0';
 $nghttp2Url = ('https://github.com/nghttp2/nghttp2/archive/v{0}.zip' -f $nghttp2Version);
 
 $curlVersion = '7.53.1';
