2adce4d422547a1c61917e524a819e665c399fd4
Don <don.j.olmstead@gmail.com>
2018-04-03 20:21:32 -0700

    Add scripts to help build WinCairoRequirements in full

---
+++ Export-Requirements.ps1
@@ -0,0 +1,32 @@
+<#
+  .Synopsis
+  Exports all the libraries required for building WebKit.
+  .Details
+  Invokes vcpkg export to package all the libraries.
+  .Parameter Triplet
+  The vcpkg triplet to use. Defaults to 'x64-windows'
+  .Parameter Libraries
+  Path to a JSON file containing the list of libraries. Defaults to 'WinCairoRequirements.json'.
+#>
+
+Param(
+  [Parameter()]
+  [string] $triplet = 'x64-windows',
+  [Parameter()]
+  [string] $libraries = 'WinCairoRequirements.json'
+)
+
+$json = Get-Content -Raw -Path $libraries | ConvertFrom-Json
+
+$arguments = @('export')
+$arguments += $json
+$arguments += '--triplet'
+$arguments += $triplet
+$arguments += '--zip'
+
+Write-Host ('vcpkg {0}' -f ($arguments -Join ' '))
+
+Start-Process -Wait -NoNewWindow `
+  -FilePath 'vcpkg.exe' `
+  -WorkingDirectory $PSScriptRoot `
+  -ArgumentList $arguments
---
+++ Remove-Requirements.ps1
@@ -0,0 +1,31 @@
+<#
+  .Synopsis
+  Removes all the source code required for building WebKit.
+  .Details
+  Invokes vcpkg remove to clean all the libraries.
+  .Parameter Triplet
+  The vcpkg triplet to use. Defaults to 'x64-windows'
+  .Parameter Libraries
+  Path to a JSON file containing the list of libraries. Defaults to 'WinCairoRequirements.json'.
+#>
+
+Param(
+  [Parameter()]
+  [string] $triplet = 'x64-windows',
+  [Parameter()]
+  [string] $libraries = 'WinCairoRequirements.json'
+)
+
+$json = Get-Content -Raw -Path $libraries | ConvertFrom-Json
+
+$arguments = @('remove')
+$arguments += $json
+$arguments += '--triplet'
+$arguments += $triplet
+
+Write-Host ('vcpkg {0}' -f ($arguments -Join ' '))
+
+Start-Process -Wait -NoNewWindow `
+  -FilePath 'vcpkg.exe' `
+  -WorkingDirectory $PSScriptRoot `
+  -ArgumentList $arguments
