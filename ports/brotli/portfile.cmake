2468f094e792d2950990ff279e0b4377c03cdf92
Don <don.j.olmstead@gmail.com>
2018-04-23 19:36:35 -0700

    Update brotli to v1.0.4

--- ports/brotli/portfile.cmake
+++ ports/brotli/portfile.cmake
@@ -1,13 +1,13 @@
 include(vcpkg_common_functions)
 
-set(BROTLI_VERSION 1.0.3)
+set(BROTLI_VERSION 1.0.4)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/brotli-${BROTLI_VERSION})
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/google/brotli/archive/v${BROTLI_VERSION}.zip"
     FILENAME "brotli-${BROTLI_VERSION}.zip"
-    SHA512 155bb074b494ece12d32fa6c97705367f08105e0f84b1d237b93840e97ca135fe12d2669d0fa9aceb81b76a8d77bcab6bfbb2646f18b26f11af56dff49ef3adc
+    SHA512 351fd272e57a90e6253bd3a97117ade6432d30b79d2b49c4fb36970eb1ab65446810574565ca90fc0111619d883048d05c99979e0e6217f97de531b8d0f74f74
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
