d7eeedd44103d64a02a64e24af4daa1005cb9786
Don <don.j.olmstead@gmail.com>
2018-08-01 19:59:33 -0700

    Add script to package requirements

---
+++ Package-Requirements.ps1
@@ -0,0 +1,18 @@
+<#
+  .Synopsis
+  Packages the requirements into a zip file.
+  .Parameter Triplet
+  The vcpkg triplet to use. Defaults to 'x64-windows'
+  .Parameter Output
+  The filename to output to. Defaults to 'WinCairoRequirements.zip'.
+#>
+
+Param(
+  [Parameter()]
+  [string] $triplet = 'x64-windows-webkit',
+  [Parameter()]
+  [string] $output = 'WinCairoRequirements.zip'
+)
+
+Write-Host ('Creating archive {0}' -f $output)
+Compress-7Zip -ArchiveFileName $output -Path ('installed/{0}' -f $triplet)
