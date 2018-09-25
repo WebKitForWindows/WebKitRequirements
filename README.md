9f4c0327e94c3ac9114ac468439c3f5b71d71c02
Don <don.j.olmstead@gmail.com>
2018-09-24 18:33:20 -0700

    Update brotli to v1.0.6

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 |---|:---:|:---:|
 | [icu](http://site.icu-project.org) | 61.1 | 03-26-2018 |
 | [zlib](https://github.com/Dead2/zlib-ng) | N/A | 03-04-2018 |
-| [brotli](https://github.com/google/brotli) | 1.0.5 | 06-27-2018 |
+| [brotli](https://github.com/google/brotli) | 1.0.6 | 09-19-2018 |
 | [libressl](https://www.libressl.org) | 2.8.0 | 08-06-2018 |
 | [nghttp2](https://nghttp2.org) | 1.33.0 | 09-02-2018 |
 | [curl](https://curl.haxx.se) | 7.61.1 | 09-05-2018 |
--- ports/brotli/portfile.cmake
+++ ports/brotli/portfile.cmake
@@ -1,13 +1,13 @@
 include(vcpkg_common_functions)
 
-set(BROTLI_VERSION 1.0.5)
+set(BROTLI_VERSION 1.0.6)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/brotli-${BROTLI_VERSION})
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/google/brotli/archive/v${BROTLI_VERSION}.zip"
     FILENAME "brotli-${BROTLI_VERSION}.zip"
-    SHA512 a2ec27cf405d66953470c1f5c411aa15935a65a8f19ee10277cf8dcbe65d7447c370e57fce69d38327c1be90b442458d4229b155154849cf95b47bc25aa2c3e7
+    SHA512 dff7d1d274808c340bce9f0224c1a479a31e3f94eda125f9b66cb072cbd8d27d0e281440040a59a3103f08cb37c86592600dc7a387a5aed95353bf9cd1f4dbce
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
