845674f7c1709ae99b4a8da1cf5328d37f0812ed
Don <don.j.olmstead@gmail.com>
2018-10-09 17:05:48 -0700

    Add x86 triplet

---
+++ triplets/x86-windows-webkit.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+# The following libraries should always be static
+if (PORT MATCHES "libwebp")
+  set(VCPKG_LIBRARY_LINKAGE static)
+elseif (PORT MATCHES "pixman")
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif ()
