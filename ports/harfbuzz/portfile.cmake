0b50663ddc94fb6a6fb953e1ddd8494908ed6efd
Don <don.j.olmstead@gmail.com>
2019-02-06 11:48:54 -0800

    Update harfbuzz to v2.3.1

--- ports/harfbuzz/portfile.cmake
+++ ports/harfbuzz/portfile.cmake
@@ -1,12 +1,12 @@
 include(vcpkg_common_functions)
 
-set(HARFBUZZ_VERSION 2.3.0)
+set(HARFBUZZ_VERSION 2.3.1)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/harfbuzz-${HARFBUZZ_VERSION})
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/harfbuzz/harfbuzz/releases/download/${HARFBUZZ_VERSION}/harfbuzz-${HARFBUZZ_VERSION}.tar.bz2"
     FILENAME "harfbuzz-${HARFBUZZ_VERSION}.tar.bz2"
-    SHA512 830c5b96384554fb6d2502713e9b2eff384dbe6aa3feb08830226944bcb07c8fb55237f389e1e6416d0942e3efba8b1ed54a49ff8c4762fec230f2404095e85a
+    SHA512 78a8f05bfcc95cfe3fc0f1a595bdc7298c9c1456db6c7ef70914051fda43f37aaff15eac75aa6922eca82d2291baeb8385e02e6aacb44ca05b4873c311a662ac
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
