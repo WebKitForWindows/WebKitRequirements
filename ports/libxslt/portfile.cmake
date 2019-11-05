99fb64631d30867b855ffede8a7edd9475bebe3b
Don <don.j.olmstead@gmail.com>
2019-11-05 13:14:39 -0800

    Add version to libxslt share dir

--- ports/libxslt/portfile.cmake
+++ ports/libxslt/portfile.cmake
@@ -44,3 +44,4 @@ vcpkg_copy_pdbs()
 # Prepare distribution
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libxslt RENAME copyright)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/libxslt/version ${LIBXSLT_VERSION})
