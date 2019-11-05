69247d7070ca721b608d062628c32d07378c2ea3
Don <don.j.olmstead@gmail.com>
2019-11-05 11:39:14 -0800

    Update openjpeg to use _archive_ex

--- ports/openjpeg/portfile.cmake
+++ ports/openjpeg/portfile.cmake
@@ -1,14 +1,20 @@
 include(vcpkg_common_functions)
 
 set(OPENJPEG_VERSION 2.3.1)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/openjpeg-${OPENJPEG_VERSION})
 
+# Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/uclouvain/openjpeg/archive/v${OPENJPEG_VERSION}.zip"
     FILENAME "openjpeg-${OPENJPEG_VERSION}.zip"
     SHA512 809cff67e34893ccc13e41383f1bb3bb1fd9640d8e0997d6ec16afbbd326c7c4436e93a2e16ee8ebb22052c88c3e5765b473905cdc4699b8ae21b0310ea48138
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+
+# Extract archive
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${OPENJPEG_VERSION}
+)
 
 # Run CMake build
 vcpkg_configure_cmake(
