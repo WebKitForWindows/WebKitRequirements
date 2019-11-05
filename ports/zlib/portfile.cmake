297e4f5b918f5f2e51538dc4270910a884605370
Don <don.j.olmstead@gmail.com>
2019-11-05 10:24:25 -0800

    Add patches into zlib usage of vcpkg_from_github

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,19 +1,18 @@
 include(vcpkg_common_functions)
 
+# Patches
+set(ZLIB_PATCHES
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0001-Remove-static-target.patch
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0002-Add-__has_declspec_attribute.patch
+)
+
 # Get from github commit until a release happens
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Dead2/zlib-ng
     REF 098f73a45e1e346b7634b6caa15675095f764dfc
     SHA512 7a7e04b84de6a9ecc9d61e6b73f03a99148c76a95ff4efc9e00e73ffc42fab89f5c8ec882ddc64345e993c4046d9caca3fbf94ac0d9af080a59904134b860b9f
-)
-
-# Apply patches
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/patches/0001-Remove-static-target.patch
-        ${CMAKE_CURRENT_LIST_DIR}/patches/0002-Add-__has_declspec_attribute.patch
+    PATCHES ${PATCHES}
 )
 
 # Run CMake build
