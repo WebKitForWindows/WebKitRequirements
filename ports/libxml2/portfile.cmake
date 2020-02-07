ce977155abe3126c5127513dc93c4535058d2fda
Don <don.j.olmstead@gmail.com>
2020-02-07 12:35:06 -0800

    Update CMake for libxml2

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -86,9 +86,12 @@ else ()
     )
 endif ()
 
+# Disable parallel configure due to configure file writing back into the
+# include directory
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS ${BUILD_OPTIONS}
 )
 
