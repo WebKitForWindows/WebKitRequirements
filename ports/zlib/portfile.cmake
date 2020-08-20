d6a7748e5ad847fd7f61e293f207d49b9b883469
Don <don.j.olmstead@gmail.com>
2020-08-20 12:38:47 -0700

    Update zlib to latest commit da1ab79

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 set(VERSION 1.2.11)
-set(REF 193d8fd7dfb7927facab7a3034daa27ad5b9df1c)
+set(REF da1ab79e1d9608d669297a9964644a6a39526be1)
 
 # Patches
 set(PATCHES
@@ -13,7 +13,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zlib-ng/zlib-ng
     REF ${REF}
-    SHA512 d78f249331622d52c862ac638cc29a6e4a9aab154103636a45fc69170d4996e911a7b2bd1e0e438891bb4d0f644fbafdd089fc13402aa605b61e9cd76a1a1552
+    SHA512 7ad18018b8dd8607670c42028d564bc6d7057da32ad56854d6e3a188b2054303885c67604fcd47d2d81c88262e6a86ef332aef9fedb36d53b504e5de4a4abeb7
     PATCHES ${PATCHES}
 )
 
