2126f303f1657462f6144c6bd49efaa28104cd56
Don <don.j.olmstead@gmail.com>
2020-10-20 16:48:07 -0700

    Add c-ares port

---
+++ ports/c-ares/portfile.cmake
@@ -0,0 +1,64 @@
+include(vcpkg_common_functions)
+
+set(VERSION 1.16.1)
+string(REPLACE "." "_" TAG ${VERSION})
+
+# Get archive
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/c-ares/c-ares/releases/download/cares-${TAG}/c-ares-${VERSION}.tar.gz"
+    FILENAME "c-ares-${VERSION}.tar.gz"
+    SHA512 4ac2a5d5c6da74eb1d6155c4eadc7127ab1b53a8d13caec41bd6172db5417a79f3ab022e77ba37d8b13da6893d7ced5fd8baf5cc3950a4154b4de8743ad31471
+)
+
+# Patches
+set(PATCHES
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0001-Adjust-CMake-for-vcpkg.patch
+)
+
+# Extract archive
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${VERSION}
+    PATCHES ${PATCHES}
+)
+
+# Run CMake build
+set(BUILD_OPTIONS
+    -DCARES_INSTALL=ON
+    -DCARES_BUILD_TESTS=OFF
+    -DCARES_BUILD_CONTAINER_TESTS=OFF
+    -DCARES_BUILD_TOOLS=OFF
+)
+
+if (${VCPKG_LIBRARY_LINKAGE} STREQUAL static)
+    set(CARES_STATIC ON)
+    set(CARES_SHARED OFF)
+else ()
+    set(CARES_STATIC OFF)
+    set(CARES_SHARED ON)
+endif ()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS 
+        ${BUILD_OPTIONS}
+        -DCARES_STATIC=${CARES_STATIC}
+        -DCARES_SHARED=${CARES_SHARED}
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+# Prepare distribution
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/c-ares RENAME copyright)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/c-ares/version ${VERSION})
+
+# The static build creates empty bin directories
+if (CARES_STATIC)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif ()
