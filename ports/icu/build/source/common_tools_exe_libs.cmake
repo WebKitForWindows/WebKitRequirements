2ccd061e002c8d8e3a91a5c887f3af47185b4e9a
Don <don.j.olmstead@gmail.com>
2020-11-04 11:58:47 -0800

    Remove old build files for icu

--- ports/icu/build/source/common_tools_exe_libs.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2018, NikitaFeodonit. All rights reserved.
-#
-## ICU build file for CMake build tools
-
-### Common executables libraries
-
-# PRIVATE
-# LIBS = $(LIBICUTOOLUTIL) $(LIBICUI18N) $(LIBICUUC) $(DEFAULT_LIBS) $(LIB_M)
-target_link_libraries(${exe_NAME} PRIVATE ${ICULIBS_TOOLUTIL} ${LIB_M})
--- ports/icu/build/source/tools/gendict/ICU-61.1-source_files.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2018, NikitaFeodonit. All rights reserved.
-#
-## ICU build file for CMake build tools
-
-target_sources(${exe_NAME}
-  PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/gendict.cpp
-)
