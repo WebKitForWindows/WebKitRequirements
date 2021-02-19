4c01f16d3fef102d7f6494500a532d1f90b088f6
Don <don.j.olmstead@gmail.com>
2021-02-19 14:35:00 -0800

    Allow building of openssl executable

--- ports/libressl/portfile.cmake
+++ ports/libressl/portfile.cmake
@@ -21,28 +21,47 @@ vcpkg_extract_source_archive_ex(
     PATCHES ${PATCHES}
 )
 
+if (tools IN_LIST FEATURES)
+    message(STATUS "Enabling tools")
+    set(LIBRESSL_APPS ON)
+else ()
+    set(LIBRESSL_APPS OFF)
+endif ()
+
 # Run CMake build
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DLIBRESSL_APPS=OFF
         -DLIBRESSL_TESTS=OFF
+    OPTIONS_RELEASE
+        -DLIBRESSL_APPS=${LIBRESSL_APPS}
+    OPTIONS_DEBUG
+        -DLIBRESSL_APPS=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 # Prepare distribution
+if (tools IN_LIST FEATURES)
+    vcpkg_copy_tools(TOOL_NAMES openssl AUTO_CLEAN)
+
+    # Empty directory created during install to house certificated
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc/ssl/certs)
+else ()
+    # Config and pem files are not installed without the apps
+    file(
+        INSTALL
+            ${SOURCE_PATH}/apps/openssl/cert.pem
+            ${SOURCE_PATH}/apps/openssl/openssl.cnf
+            ${SOURCE_PATH}/apps/openssl/x509v3.cnf
+        DESTINATION ${CURRENT_PACKAGES_DIR}/etc/ssl
+    )
+endif ()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/man)
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libressl RENAME copyright)
-file(
-    INSTALL 
-        ${SOURCE_PATH}/apps/openssl/cert.pem
-        ${SOURCE_PATH}/apps/openssl/openssl.cnf
-        ${SOURCE_PATH}/apps/openssl/x509v3.cnf
-    DESTINATION ${CURRENT_PACKAGES_DIR}/etc/ssl
-)
 file(WRITE ${CURRENT_PACKAGES_DIR}/share/libressl/version ${VERSION})
