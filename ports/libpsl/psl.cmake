972f6099fed73fdbf75a4870885da5984121c01a
Don <don.j.olmstead@gmail.com>
2021-03-31 08:58:47 -0700

    Update public suffix list to e4051df5

--- ports/libpsl/psl.cmake
+++ ports/libpsl/psl.cmake
@@ -1,10 +1,10 @@
 # Download PSL file
-# Up to date as of 08-17-2020
+# Up to date as of 2020-03-30
 vcpkg_from_github(
     OUT_SOURCE_PATH PSL_SOURCE_PATH
     REPO publicsuffix/list
-    REF 529308c7668f67f2206628314d40d7eef0c2c261
-    SHA512 af37a04df0a87c08fb32590a09b6ca70f54e3189033fc3ce80adcd8755b8719701bf0444ec3999e208ac83fa318cd8c94a13b74ec0d0032b7e698eb229a3730b
+    REF e4051df5af7d0c38b66cbc075205d5660feed7b2
+    SHA512 c5e397a33d96d62077970874e23826ecefc4c17d7bbe9e83db49af43deccc6eda4cbb7b76212468a7235644af42f494bdbaaa6b9f9a86447fa61159050cee865
     HEAD_REF master
 )
 
