706eb454b6a73ab4810704189c1fa00cde7ebaa1
Don <don.j.olmstead@gmail.com>
2022-03-03 15:49:40 -0800

    Update libxslt to v1.1.35

--- ports/libxslt/portfile.cmake
+++ ports/libxslt/portfile.cmake
@@ -1,10 +1,20 @@
-set(VERSION 1.1.34)
+set(VERSION_MAJOR 1)
+set(VERSION_MINOR 1)
+set(VERSION_PATCH 35)
+set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH})
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://xmlsoft.org/sources/libxslt-${VERSION}.tar.gz"
-    FILENAME "libxslt-${VERSION}.tar.gz"
-    SHA512 1516a11ad608b04740674060d2c5d733b88889de5e413b9a4e8bf8d1a90d712149df6d2b1345b615f529d7c7d3fa6dae12e544da828b39c7d415e54c0ee0776b
+    URLS "https://download.gnome.org/sources/libxslt/${VERSION_MAJOR}.${VERSION_MINOR}/libxslt-${VERSION}.tar.xz"
+    FILENAME "libxslt-${VERSION}.tar.xz"
+    SHA512 9dd4a699235f50ae9b75b25137e387471635b4b2da0a4e4380879cd49f1513470fcfbfd775269b066eac513a1ffa6860c77ec42747168e2348248f09f60c8c96
+)
+
+# Patches
+set(PATCHES
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0001-Adjust-CMake-for-vcpkg.patch
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0002-Add-ICU-build-option.patch
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0003-Add-tooling-build-option.patch
 )
 
 # Extract archive
@@ -15,25 +25,19 @@ vcpkg_extract_source_archive_ex(
     PATCHES ${PATCHES}
 )
 
-# Add CMake sources
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/build/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/build/config.h.cmake.in DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/build/libxslt-config.cmake.in DESTINATION ${SOURCE_PATH})
-
 # Run CMake build
 set(BUILD_OPTIONS
-    # Use CMAKE_DISABLE_FIND_PACKAGE_XXX=TRUE to disable dependencies in libxslt
-    # Otherwise libxslt will attempt to use it if available
-    -DCMAKE_DISABLE_FIND_PACKAGE_Iconv=TRUE
-    -DCMAKE_DISABLE_FIND_PACKAGE_Python=TRUE
-    -DCMAKE_DISABLE_FIND_PACKAGE_Thread=TRUE
-
     # Options
-    -DLIBXSLT_WITH_DEBUGGER=OFF
     -DLIBXSLT_WITH_CRYPTO=OFF
+    -DLIBXSLT_WITH_DEBUGGER=OFF
+    -DLIBXSLT_WITH_ICU=ON
     -DLIBXSLT_WITH_MEM_DEBUG=OFF
     -DLIBXSLT_WITH_MODULES=OFF
     -DLIBXSLT_WITH_PROFILER=OFF
+    -DLIBXSLT_WITH_PYTHON=OFF
+    -DLIBXSLT_WITH_TESTS=OFF
+    -DLIBXSLT_WITH_TOOLS=OFF
+    -DLIBXSLT_WITH_THREADS=OFF
     -DLIBXSLT_WITH_TRIO=OFF
     -DLIBXSLT_WITH_XSLT_DEBUG=OFF
 )
