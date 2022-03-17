c587c5bf73b07be1564c78e6e28aaef91bad672e
Christopher Reid <chris.reid@sony.com>
2022-03-17 15:52:24 -0700

    Update psl file and pull in patches from libpsl (#82)

--- ports/libpsl/psl.cmake
+++ ports/libpsl/psl.cmake
@@ -1,10 +1,10 @@
 # Download PSL file
-# Up to date as of 2020-03-30
+# Up to date as of 2022-03-16
 vcpkg_from_github(
     OUT_SOURCE_PATH PSL_SOURCE_PATH
     REPO publicsuffix/list
-    REF e4051df5af7d0c38b66cbc075205d5660feed7b2
-    SHA512 c5e397a33d96d62077970874e23826ecefc4c17d7bbe9e83db49af43deccc6eda4cbb7b76212468a7235644af42f494bdbaaa6b9f9a86447fa61159050cee865
+    REF c1f376165d9591ba47b8a01b11cbc5e3ffbd81b2
+    SHA512 5b7698868ac4e3a06e4ad8d04b6cffbadd6234bbb79caca6e55dd0088248e6f6ad39b7115eb6a0c0c8fc06ca8b88fe485c0788fc9d49259e65f0cb8d0a24a6f1
     HEAD_REF master
 )
 
