087d24c4783fa6e633185a2230156688ca96bcf0
Don <don.j.olmstead@gmail.com>
2022-09-24 19:02:08 -0700

    Update harfbuzz to v5.2.0

--- ports/harfbuzz/portfile.cmake
+++ ports/harfbuzz/portfile.cmake
@@ -1,16 +1,17 @@
-set(VERSION 5.1.0)
+set(VERSION 5.2.0)
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/harfbuzz/harfbuzz/releases/download/${VERSION}/harfbuzz-${VERSION}.tar.xz"
     FILENAME "harfbuzz-${VERSION}.tar.xz"
-    SHA512 452c4236ef997db2a32c5ac32d3b619c5fa9b5691cde935092b32581387de8d161ab1ba78dd9fa02c36ce553f0f1fdd5564132ec81cd7b863af6d3be96cbf979
+    SHA512 3c95ae9916dfc283714b936988726c0b391f634e22a62d148c201104178ee229c48e49cc82961d728a5c8bc8af06978eb172b35acb552c969bc2385892380d30
 )
 
 # Patches
 set(PATCHES
     ${CMAKE_CURRENT_LIST_DIR}/patches/0001-Use-find_package-for-ICU.patch
     ${CMAKE_CURRENT_LIST_DIR}/patches/0002-Adjust-CMake-for-vcpkg.patch
+    ${CMAKE_CURRENT_LIST_DIR}/patches/0003-Remove-icu-uc-from-pkgconfig.patch
 )
 
 # Extract archive
