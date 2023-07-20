fcb9cfe6821d21251037fc46f4d7f75ee84d4379
mokjasmine <jasmine.mok@sony.com>
2023-07-20 15:57:54 -0700

    Update nghttp3 to v0.13.0

--- ports/nghttp3/portfile.cmake
+++ ports/nghttp3/portfile.cmake
@@ -1,10 +1,10 @@
-set(VERSION 0.11.0)
+set(VERSION 0.13.0)
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/ngtcp2/nghttp3/releases/download/v${VERSION}/nghttp3-${VERSION}.tar.bz2"
     FILENAME "nghttp3-${VERSION}.tar.bz2"
-    SHA512 bb168e12a3456f503b2364717a2b8770b164de6fbe91fba1c93b8fabd6435d544570ea01ff4bdbf83cdc472098817dfd9376dd56acdac36a4c7d8c909ea8f5c9
+    SHA512 c0597d1ea277624c0aba70f5a5fc3f7fd88749788c343e5b5bd480c69b26d853313fdc1a6085fcfe28a20b21d97506199123360403b70ee5f983602f1973028c
 )
 
 # Extract archive
