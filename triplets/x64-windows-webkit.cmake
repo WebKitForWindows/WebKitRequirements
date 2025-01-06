e82dce658bd08faed37365e6b3b7d0cae0650521
Don Olmstead <don.j.olmstead@gmail.com>
2025-01-06 10:16:22 -0800

    Add x64-windows-webkit triplet
    Specify the linkage of the dependent libraries.
    
    This is a precursor to use zlib-ng instead of zlib.

---
+++ triplets/x64-windows-webkit.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+# The following libraries should always be static
+if (PORT STREQUAL "highway")
+    set(VCPKG_LIBRARY_LINKAGE static)
+elseif (PORT STREQUAL "pixman")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif ()
